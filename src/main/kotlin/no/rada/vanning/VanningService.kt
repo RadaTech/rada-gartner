974f74b246a285803107fb64d3a0ad7e0af4e6ca
SanderOpperud <sanderopperud@hotmail.com>
2022-10-24 14:23:22 +0200

    Fiks

--- src/main/kotlin/no/rada/vanning/VanningService.kt
+++ src/main/kotlin/no/rada/vanning/VanningService.kt
@@ -6,8 +6,7 @@ class VanningService {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     internal fun vannPlante(mengdeVann: Double) {
-        if (mengdeVann < MINIMUM_MENGDE_VANN) throw Exception("Forsøker å vanne med for lite vann. Må være minimum $MINIMUM_MENGDE_VANN dl. Forespørsel = '$mengdeVann'")
-
+        if (mengdeVann < MINIMUM_MENGDE_VANN) throw Exception("For lite vann. Må være minimum $MINIMUM_MENGDE_VANN dl. Forespørsel = '$mengdeVann'")
         logger.info("Vanner plante med $mengdeVann dl")
     }
 
